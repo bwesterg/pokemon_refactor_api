258a5cd1ea5fda086977dbf5e22251bf0c7b0265
bwesterg <benton.westergaard@gmail.com>
2021-04-27 17:15:40 -0600

    added pokeon index route with action using rest client to poke api.

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,20 @@
+class PokemonsController < ApplicationController
+
+  def index
+    response = RestClient.get "https://pokeapi.co/api/v2/pokemon?offset=0&Limit=20"
+    result = JSON.parse response
+    api_pokemons = result["results"]
+
+    pokemons = []
+
+    pokemons = api_pokemons.reduce([]) do |poke_list, api_pokemon|
+      poke_response = RestClient.get api_pokemon["url"]
+      poke_result = JSON.parse poke_response
+      poke_list << poke_result
+      poke_list
+    end
+
+    render json: pokemons
+  end
+
+end
